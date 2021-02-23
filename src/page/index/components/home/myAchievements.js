4142881210d14dec436d20968f5fb42ec29f7973
majingzhe <majz@zts.com.cn>
2021-02-23 20:53:07 +0800

    我的绩效

--- src/page/index/components/home/myAchievements.js
+++ src/page/index/components/home/myAchievements.js
@@ -16,20 +16,22 @@ class MyAchievements extends Component {
         })
     }
 
-
     render() {
+        const width = this.state.achievementsObj.currentValue/this.state.achievementsObj.targetValue*100 + '%'
+
         return (
             <div className="myAchievements">
                 <div className="title">我的绩效</div>
 
                 <div className="content">
                     <div className="box">
-                        <p>业务净收入</p>
-                        <p>0元/100元</p>
+                        <p>业务净收入
+                            {this.state.achievementsObj.excess === '1' && <span className="ce">超额完成</span>}
+                        </p>
+                        <p>{this.state.achievementsObj.currentValue}元/{this.state.achievementsObj.targetValue}元</p>
                         <div className="progress">
-                            <img src={require('../../../../assets/img/home/line.png')} />
+                            <img src={require('../../../../assets/img/home/line.png')} style={{'width': width}}/>
                         </div>
-
                     </div>
                 </div>
             </div>
--- src/page/index/home.js
+++ src/page/index/home.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 import MyAchievements from '../index/components/home/myAchievements'
+import '../../style/index/home.scss'
 
 class Home extends Component {
     render() {
+        const iconList = [{
+            name: '晨会聚焦'
+        },{
+            name: '开户追踪'
+        },{
+            name: '打新日历'
+        }]
+
+
         return (
             <div>
+                <div className="functionIcon">
+                    <ul>
+                        {
+                            iconList.map( (item,index) => {
+                            return (
+                                <li key={index}>{item.name}</li>
+                            )})
+                        }
+                    </ul>
+
+                </div>
+
                 <MyAchievements/>
             </div>
         );
